aba8c5d011dd9f91b4ad940115a02dd2fcee5df6
Patrick Stoffel <patrick_stoffel@hotmail.com>
2017-05-09 18:14:19 +0200

    modify iterator.js

--- src/exercises/iterator.js
+++ src/exercises/iterator.js
@@ -2,6 +2,15 @@
 // Implementiere einen Array Iterator, der einen Array rückwärts interiert.
 // Versuche zwei Varianten zu finden.
 
+let forFun = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+forFun.reverse();
+
+for (let value of forFun) {
+  value
+}
+
+
 // 2 
 // Schreibe eine Funktion forFun, welche
 // - ein Objekt welches Symbol.iterator implementiert
